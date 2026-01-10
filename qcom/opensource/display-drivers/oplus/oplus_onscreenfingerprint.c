308980f494ac0f6adcd82c99e248d0a5d9f20568
grepfox <grepfox@tutamail.com>
2026-01-10 15:51:32 +0530

    fixup! display-drivers: oplus_onscreenfingerprint: Handle finger up event in touchpanel notifier
    Change-Id: If9bf5c745a2e1351a35534a07f2506c3fe7b883d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- qcom/opensource/display-drivers/oplus/oplus_onscreenfingerprint.c
+++ qcom/opensource/display-drivers/oplus/oplus_onscreenfingerprint.c
@@ -4374,7 +4374,8 @@ int oplus_ofp_touchpanel_event_notifier_call(struct notifier_block *nb, unsigned
 			} else if (tp_event->touch_state == 0) {
 				OFP_INFO("tp touchup\n");
 				if (p_oplus_ofp_params->need_to_update_lhbm_pressed_icon_gamma_nt37707) {
-					if (oplus_ofp_notify_fp_press(&tp_event->touch_state))
+					uint32_t fp_press = 0;
+					if (oplus_ofp_notify_fp_press(&fp_press))
 						OFP_INFO("failed to notify fp up event");
 				}
 			}
@@ -5220,6 +5221,8 @@ int oplus_ofp_notify_fp_press(void *buf)
 			/* Avoid using thread to exit AOD for blind fingerprint decoding in 60hz AOD of video mode */
 			if (oplus_ofp_video_mode_aod_fod_is_enabled()) {
 				oplus_ofp_aod_off_handle(display);
+			} else {
+				oplus_ofp_aod_off_set();
 			}
 		} else {
 			oplus_ofp_aod_off_set();
